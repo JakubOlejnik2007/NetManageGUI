79dc969369b109e7656c4aa4949876420e5eaf37
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-11-04 12:56:44 +0100

    Removed unused comments

--- CommandsList.py
+++ CommandsList.py
@@ -5,21 +5,16 @@ class CommandList(QWidget):
     def __init__(self):
         super().__init__()
 
-        # Ustawienie layoutu
         self.layout = QVBoxLayout()
 
-        # Tworzenie etykiety
         self.label = QLabel('Lista komend')
         self.layout.addWidget(self.label)
 
-        # Tworzenie przycisku
         self.button = QPushButton('Kliknij mnie')
-        self.button.clicked.connect(self.change_text)  # Połączenie sygnału z metodą
+        self.button.clicked.connect(self.change_text)
         self.layout.addWidget(self.button)
 
-        # Ustawienie layoutu dla widgetu
         self.setLayout(self.layout)
 
     def change_text(self):
-        # Zmiana tekstu etykiety
         self.label.setText('Tekst został zmieniony!')
