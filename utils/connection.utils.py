24e1460cb310408723ed1017976c821dc4cb608e
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-11-21 10:59:24 +0100

    Removed unused file prepared for testing connections

--- utils/connection.utils.py
+++
@@ -1,7 +0,0 @@
-from NetManage.utils import COM_CONNECTION, SSHTEL_CONNECTION, TFTP_CONNECTION
-
-
-def testConnection(connection: COM_CONNECTION | SSHTEL_CONNECTION | TFTP_CONNECTION):
-    connect = connection.getNetmikoConnDict()
-
-
