71cc64e98a5b82a616ed175b753e6f0a0b48c7d5
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2024-11-21 10:55:45 +0100

    Added BASE_DIR path to main app folder. Changes in working directory doesn't affect working of app

---
+++ utils/consts.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+SUPPORTED_DEVICES = [
+    'cisco_ios', 'cisco_xe', 'cisco_asa', 'cisco_nxos', 'cisco_iosxr', 'arista_eos', 'juniper',
+    'hp_procurve', 'dell_force10', 'brocade', 'fortinet', 'mikrotik', 'huawei', 'checkpoint', 'paloalto'
+]
+
+BASE_DIR = (
+    os.path.dirname(os.path.dirname(sys.executable))
+    if hasattr(sys, "frozen")
+    else os.path.dirname(os.path.dirname(__file__))
+)
+CONNECTIONS_DIR = os.path.join(BASE_DIR, "connections")
+COMMANDS_DIR = os.path.join(BASE_DIR, "commands")
+ASSETS_DIR = os.path.join(BASE_DIR, "assets")
